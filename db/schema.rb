134b13fdb7ffc3ba24ece5058e23155cbd979ea3
jasminepal <jasmine4pal@gmail.com>
2023-08-20 16:37:15 +0530

    updated code

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_20_054758) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_20_062859) do
   create_table "blogs", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -18,7 +18,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_20_054758) do
     t.string "title"
     t.text "content"
     t.string "image"
-    t.string "avatar"
     t.index ["user_id"], name: "index_blogs_on_user_id"
   end
 
@@ -30,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_20_054758) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
